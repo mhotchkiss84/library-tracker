f1c76b2c5a090dc452cdfe4a94fd824577da02b2
Mike Hotchkiss <sxs.linux@hotmail.com>
2020-05-14 11:45:31 -0400

    Created domPrinter function for printing form and exported. Also added some CSS.

---
+++ src/scripts/domPrinter.js
@@ -0,0 +1,26 @@
+// When the page loads, users should see a form with the following fileds:
+// Title
+// Author
+// ISBN Number
+// When the user enters information into the fields and clicks a "Save" button, the new book should be saved to the database.
+
+// Creating a object to store the dom printer functions
+const domPrinter = {
+    //Creating a function to print the form to the DOM
+    printForm: () =>{
+        document.querySelector(`#form-container`).innerHTML = `
+        <h3>Submit a new book</h3>
+        <form id="create-entry">
+        <label for="title">Title</>
+        <input type="text" id="book-title">
+        <label for="author">Author</>
+        <input type="text" id="book-author">
+        <label for="isbn">ISBN Number</>
+        <input type="text" id="book-isbn">
+        </form>
+        <button id="submit-btn">Submit</button>
+        `
+    }
+}
+// Exporting the domPrinter functions
+export default domPrinter
---
+++ src/scripts/main.js
@@ -0,0 +1,4 @@
+// Importing the domPrinter functions
+import domPrinter from './domPrinter.js'
+// Calling domPrinter printForm to load the form on the page
+domPrinter.printForm()
